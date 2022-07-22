edec44b1b1c22fa3b528583470d65c11b11b2109
Phil McGarty <philmcgarty@hotmail.com>
2022-07-22 00:11:52 -0400

    converted to props for book v film section

--- client/src/components/ThisWeekSquabble.jsx
+++ client/src/components/ThisWeekSquabble.jsx
@@ -2,43 +2,44 @@ import React from "react";
 import lotrBookCard from '../images/LOTR_Book.webp';
 import vsGraphic from '../images/vs-img.png';
 import lotrFilmCard from '../images/LOTR-movie-resized.jpg';
+import Card from "./Card";
 
 const ThisWeekSquabble = () => {
+
+    const cards = [
+        {   
+            mediaFormat: 'Book',
+            mediaFormatCaps: 'BOOK',
+            typeClass: 'the-book',
+            imageName: lotrBookCard,
+            typeId: 'vote-book',
+            buttonClass: 'btn-danger'
+        },
+        {
+            mediaFormat: 'Movie',
+            mediaFormatCaps: 'MOVIE',
+            typeClass: 'the-movie',
+            imageName: lotrFilmCard,
+            typeId: 'vote-movie',
+            buttonClass: 'btn-primary'
+        }
+    ]
+
     return (
         // {/* <!-- Book vs Movie container --> */}
         <div className="row text-center">
 
         {/* <!-- Book card --> */}
-        <div className="card col text-center shadow-lg" style={{width: "18rem"}}>
-
-            <div className="card-body">
-                <h4 className="card-title the-book">The Book</h4>
-                <img src={lotrBookCard} className="card-img" />
-
-                <div className="card-body d-grid" id="vote-book">
-                    <a href="#" className="btn btn-danger">Vote for the BOOK</a>
-                </div>
-            </div>
-
-        </div>
-
+        <Card props={cards[0]}/>
+        
         {/* <!-- vs graphic --> */}
         <div className="col content">
             <img src={vsGraphic} className="img-fluid" />
         </div>
 
         {/* <!-- Movie card --> */}
-        <div className="card col text-center shadow-lg" style={{width: "18rem"}}>
-
-            <div className="card-body">
-                <h4 className="card-title the-movie">The Movie</h4>
-                <img src={lotrFilmCard} className="card-img" />
-
-            </div>
-            <div className="card-body d-grid" id="vote-movie">
-                <a href="#" className="btn btn-primary">Vote for the MOVIE</a>
-            </div>
-        </div>
+        <Card props={cards[1]}/>
+        
 
     </div>
     )
