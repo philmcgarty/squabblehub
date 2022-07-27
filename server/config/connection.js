2d704a162073970ee67a99ccbd958d35b602f996
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-27 13:12:15 -0400

    test parameter change config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/squabble-hub', {
+  process.env.MONGODB_URI || 'mongodb://localhost/squabble-hub', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
