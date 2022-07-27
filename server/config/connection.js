43a71b17fd4a1f8fc873c7e4d23193a0aff85b84
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-27 12:52:03 -0400

    test config change

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost:27017/squabble-hub', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
