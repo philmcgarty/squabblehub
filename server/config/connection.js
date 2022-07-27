07efa7b71717e21a12e40964140858017bc81bf8
IsaiasQB <isaias.qb@hotmail.com>
2022-07-27 01:12:50 -0400

    all change to server/connection.js
    Signed-off-by: IsaiasQB <isaias.qb@hotmail.com>

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/squabblehub', {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/squabblehub', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
