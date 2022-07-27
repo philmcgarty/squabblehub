ea14ce4d184ab632f2d98a8588426bc25601223f
Phil McGarty <philmcgarty@hotmail.com>
2022-07-26 22:16:15 -0400

    modals displaying

--- client/src/components/modals/AddMovieCommentModal.jsx
+++ client/src/components/modals/AddMovieCommentModal.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
-const AddBookCommentModal = (props) => {
-  if (!props.show){
+const AddMovieCommentModal = (props) => {
+  if (!props.showMovie){
     return null
   }
 
   return (
-        // <!-- Modal to Add BOOK Comment -->
-    <div className="new-modal" id="add-book-comment-modal" tabIndex="-1" aria-labelledby="exampleModalLabel" onClick={props.onClose}>
+        // <!-- Modal to Add MOVIE Comment -->
+    <div className="new-modal" id="add-movie-comment-modal" tabIndex="-1" aria-labelledby="exampleModalLabel" onClick={props.onClose}>
       <div className="modal-dialog">
         <div className="modal-content" onClick={e => e.stopPropagation()}>
           <div className="modal-header">
-            <h4 className="modal-title m-2 p-2 text-center the-book" id="add-book-comment-modal-btn">Add your comment</h4>
+            <h4 className="modal-title m-2 p-2 text-center the-movie" id="add-movie-comment-modal-btn">Add your comment</h4>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={props.onClose}></button>
           </div>
           <div className="form-group p-3">
-            <label htmlFor="new-comment">Comment on the BOOK here:</label>
+            <label htmlFor="new-comment">Comment on the MOVIE here:</label>
             <textarea className="form-control" id="new-comment" rows="3"></textarea>
           </div>
           <div className="modal-footer">
@@ -29,4 +29,4 @@ const AddBookCommentModal = (props) => {
     )
 }
 
-export default AddBookCommentModal;
+export default AddMovieCommentModal;
