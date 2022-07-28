bb7a360f2fef2644625b5b0e9d2951632110c08f
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-28 13:02:48 -0400

    redirect to home page if no user logged in while accessing profile page directly

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,29 +1,43 @@
 import React from "react";
+import { Navigate } from 'react-router-dom';
+
 import NextSquabblePoll from "../components/NextSquabblePoll";
 import SuggestionForm from "../components/SuggestionForm";
 import SuggestionHistory from "../components/SuggestionHistory";
-import { QUERY_POLLS } from '../utils/queries';
+
+import { QUERY_POLLS, QUERY_USER_ME } from '../utils/queries';
 import { useQuery } from '@apollo/client';
+import Auth from '../utils/auth'
+
 const Profile = () => {
+    
+
+  
 
     const { loading, data } = useQuery(QUERY_POLLS);
     const pollData = data?.polls[0] || [];
     
-    return (
-        <div>
-            {loading ? ( <h3 className="text-center">Loading...</h3> ) : (
-           <NextSquabblePoll pollData={pollData}/>
-            )}
-           <section>
-                <div className ="container display-grid mt-3">
-                    <div className="row">
-                        <SuggestionForm />
-                        <SuggestionHistory />
+    // navigate to personal profile page if username is the logged-in user's if not redirect to home page
+     if (!Auth.loggedIn()) {
+     return <Navigate to="/home" />;
+    }
+    else {
+    
+        return (
+            <div>
+                {loading ? ( <h3 className="text-center">Loading...</h3> ) : (
+                <NextSquabblePoll pollData={pollData}/>
+                )}
+                <section>
+                    <div className ="container display-grid mt-3">
+                        <div className="row">
+                            <SuggestionForm />
+                            <SuggestionHistory />
+                        </div>
                     </div>
-                </div>
-            </section>
-        </div>
-    )
-};
-
+                </section>
+            </div>
+        )
+    };
+}
 export default Profile;
